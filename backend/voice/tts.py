aab0a58177e181297fa5b2674925ff6e06b37664
Dev Popli <d1e1v1234@gmail.com>
2026-08-13 15:28:20 +0530

    Changed tts model to low

--- backend/voice/tts.py
+++ backend/voice/tts.py
@@ -10,7 +10,7 @@ class TextToSpeech:
         model_path = (
             Path(__file__).parent.parent
             / "models"
-            / "en_US-ryan-medium.onnx"
+            / "en_US-ryan-low.onnx"
         )
 
         self.voice = PiperVoice.load(
